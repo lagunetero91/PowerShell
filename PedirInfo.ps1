24ee369664efeda1136c4a928a827d304dd3bc61
David Leon Ortega <david_lagu@hotmail.com>
2017-02-23 14:28:21 +0000

    Pedir información
    Script de Power Shell que solicita por consola información al usuario.

---
+++ PedirInfo.ps1
@@ -0,0 +1,4 @@
+$nom = Read-Host "Introduzca su nombre"
+$ape = Read-Host "Introduzca su apellido"
+$tel = Read-Host "Introduzca su teléfono"
+Write-Output "Nombre: " $nom "Apellido: " $ape "tlf: " $tel  
