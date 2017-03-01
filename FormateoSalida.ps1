3a8b814bade032ed0a42ca1f12587e6149d3c664
David Leon Ortega <david_lagu@hotmail.com>
2017-03-01 11:01:08 +0000

    Formateo de salida
    Scrip que hace uso del formato de salida.

---
+++ FormateoSalida.ps1
@@ -0,0 +1,12 @@
+﻿#Formato normal.
+Get-ChildItem > C:\result.txt
+
+#Formato lista
+Get-ChildItem | Format-List -Property name,length 
+
+
+#Formato tabla.
+Get-ChildItem | Format-Wide -Column 4 
+
+#Formato tabla con propiedas de elementos
+Get-ChildItem | Format-Table -Property name,Length
